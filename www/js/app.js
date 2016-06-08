a9c96212531b3bb2e26f05cf454023e1aad1104f
esafif <esafif637@gmail.com>
2016-06-09 02:26:20 +0700

    update

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,35 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+.state('login', {
+   url: '/login',
+   templateUrl: 'templates/login.html',
+   controller: 'loginCtrl'
+  })
+    
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
